28d4a0b8c6ef11f5bdac9f497a99dcce10e2d01e
SimplyExisting <96895831+SimplyExisting@users.noreply.github.com>
2025-04-15 18:47:01 -0500

    Redid the colors of htings
    Redoing colors

--- js/base.js
+++ js/base.js
@@ -1,7 +1,5 @@
-
-document.getElementsById("formID").addEventListener("submit", function(event) {
-        event.preventDefault(); // Stops the form from actually submitting
-        window.location.href = "submitted.html"; // Redirects to your custom page
+document.getElementsById("formID").addEventListener('submit', function() {
+        window.location.href = 'submitted.html'; // Redirects to your custom page
 });
 
 
